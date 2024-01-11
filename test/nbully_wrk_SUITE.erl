26fddee839669ded937f6a6e6c2abdefeac52f55
Marcelino Alberdi Pereira <marcelino.alberdi@nomasystems.com>
2024-01-11 10:14:45 +0100

    fix: Fixed job.strategy.matrix values

--- test/nbully_wrk_SUITE.erl
+++ test/nbully_wrk_SUITE.erl
@@ -1,4 +1,4 @@
-%%% Copyright 2024 Nomasystems, S.L. http://www.nomasystems.com
+%% Copyright 2024 Nomasystems, S.L. http://www.nomasystems.com
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
